1b650a1562f10cb4bbef6bf64b2b767522ff5afd
hzn1995 <1498141625@qq.com>
2018-12-27 09:50:05 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,1 @@
-//require("./gulp");
-const gulp = require('gulp');
-// 获取 uglify 模块（用于压缩 JS）
-const uglify = require('gulp-uglify');
-// 压缩 js 文件
-// 在命令行使用 gulp script 启动此任务
-gulp.task('jscompress', function() {
-    // 1. 找到文件
-    return gulp.src('js/lib.js')
-    // 2. 压缩文件
-        .pipe(uglify())
-        // 3. 另存压缩后的文件
-        .pipe(gulp.dest('dist/js'));
-});
+require("./gulp");
